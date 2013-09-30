cf118dc7858536043375f4b5dcbfe09e2dd9e122
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-09-30 17:14:10 +0200

    This should work but it seems that some methods are not found (for example: ' VFY: unable to resolve direct method 9355: Lcom/fasterxml/jackson/dataformat/xml/XmlFactory;.<init> (Lcom/fasterxml/jackson/core/ObjectCodec;Ljavax/xml/stream/XMLInputFactory;Ljavax/xml/stream/XMLOutputFactory;)V')

--- src/main/java/net/tirasa/odatajclientandroid/Main.java
+++ src/main/java/net/tirasa/odatajclientandroid/Main.java
@@ -10,10 +10,9 @@ import android.widget.EditText;
 import com.msopentech.odatajclient.engine.communication.request.retrieve.ODataEntityRequest;
 import com.msopentech.odatajclient.engine.communication.request.retrieve.ODataRetrieveRequestFactory;
 import com.msopentech.odatajclient.engine.data.ODataEntity;
+import com.msopentech.odatajclient.engine.data.metadata.EdmMetadata;
 import com.msopentech.odatajclient.engine.format.ODataPubFormat;
 import com.msopentech.odatajclient.engine.uri.ODataURIBuilder;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 public class Main extends Activity implements OnClickListener {
 
@@ -38,54 +37,48 @@ public class Main extends Activity implements OnClickListener {
             final StringBuilder text = new StringBuilder();
 
             try {
-//                final EdmMetadata metadata = ODataRetrieveRequestFactory.getMetadataRequest(
-//                        "http://services.odata.org/v3/(S(ile3zwcmuiizgc0tozjn4202))/OData/OData.svc").
-//                        execute().getBody();
-//
-//                text.append("Metadata schema namespace: ").append(metadata.getSchema(0).getNamespace()).
-//                        append('\n').append('\n');
+                final EdmMetadata metadata = ODataRetrieveRequestFactory.getMetadataRequest(
+                        "http://services.odata.org/v3/(S(ile3zwcmuiizgc0tozjn4202))/OData/OData.svc").
+                        execute().getBody();
+
+                text.append("Metadata schema namespace: ").append(metadata.getSchema(0).getNamespace()).
+                        append('\n').append('\n');
 
                 final ODataEntityRequest req = ODataRetrieveRequestFactory.getEntityRequest(new ODataURIBuilder(
                         "http://services.odata.org/v3/(S(ile3zwcmuiizgc0tozjn4202))/OData/OData.svc").
                         appendEntitySetSegment("Products").appendKeySegment(0).build());
                 req.setFormat(ODataPubFormat.ATOM);
 
-                try {
-                    final ODataEntity product = req.execute().getBody();
+                final ODataEntity product = req.execute().getBody();
 
-                    text.append("ID: ").
-                            append(product.getProperty("ID").getValue().asPrimitive().toString()).
+                text.append("ID: ").
+                        append(product.getProperty("ID").getValue().asPrimitive().toString()).
+                        append('\n');
+
+                if (product.getProperty("Name") != null) {
+                    text.append("Name: ").
+                            append(product.getProperty("Name").getValue().asPrimitive().toString()).
                             append('\n');
-
-                    if (product.getProperty("Name") != null) {
-                        text.append("Name: ").
-                                append(product.getProperty("Name").getValue().asPrimitive().toString()).
-                                append('\n');
-                    }
-                    if (product.getProperty("Description") != null) {
-                        text.append("Description: ").
-                                append(product.getProperty("Description").getValue().asPrimitive().toString()).
-                                append('\n');
-                    }
-                    if (product.getProperty("ReleaseDate") != null) {
-                        text.append("Release date: ").
-                                append(product.getProperty("ReleaseDate").getValue().asPrimitive().toString()).
-                                append('\n');
-                    }
-                    if (product.getProperty("Rating") != null) {
-                        text.append("Rating: ").
-                                append(product.getProperty("Rating").getValue().asPrimitive().toString()).
-                                append('\n');
-                    }
-                    if (product.getProperty("Price") != null) {
-                        text.append("Price: ").
-                                append(product.getProperty("Price").getValue().asPrimitive().toString()).
-                                append('\n');
-                    }
-                } catch (Throwable t) {
-                    StringWriter sw = new StringWriter();
-                    t.printStackTrace(new PrintWriter(sw));
-                    text.append(sw.toString());
+                }
+                if (product.getProperty("Description") != null) {
+                    text.append("Description: ").
+                            append(product.getProperty("Description").getValue().asPrimitive().toString()).
+                            append('\n');
+                }
+                if (product.getProperty("ReleaseDate") != null) {
+                    text.append("Release date: ").
+                            append(product.getProperty("ReleaseDate").getValue().asPrimitive().toString()).
+                            append('\n');
+                }
+                if (product.getProperty("Rating") != null) {
+                    text.append("Rating: ").
+                            append(product.getProperty("Rating").getValue().asPrimitive().toString()).
+                            append('\n');
+                }
+                if (product.getProperty("Price") != null) {
+                    text.append("Price: ").
+                            append(product.getProperty("Price").getValue().asPrimitive().toString()).
+                            append('\n');
                 }
 
                 return text.toString();
